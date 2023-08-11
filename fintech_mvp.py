52883b77b737a2802127d7734cd9c676f6daac12
Adarsh Shirawalmath <114558126+adarshxs@users.noreply.github.com>
2023-08-11 22:32:05 +0530

    Update fintech_mvp.py

--- fintech_mvp.py
+++ fintech_mvp.py
@@ -31,7 +31,45 @@ def ask_claude(stock_info, query):
 
 def fintech_app():
     st.title("Algabay AI")
-
+    Trading_view_ticker_tape = """
+    <!-- TradingView Widget BEGIN -->
+    <div class="tradingview-widget-container">
+    <div class="tradingview-widget-container__widget"></div>
+    <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+    <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
+    {
+    "symbols": [
+        {
+        "proName": "FOREXCOM:SPXUSD",
+        "title": "S&P 500"
+        },
+        {
+        "proName": "FOREXCOM:NSXUSD",
+        "title": "US 100"
+        },
+        {
+        "proName": "FX_IDC:EURUSD",
+        "title": "EUR to USD"
+        },
+        {
+        "proName": "BITSTAMP:BTCUSD",
+        "title": "Bitcoin"
+        },
+        {
+        "proName": "BITSTAMP:ETHUSD",
+        "title": "Ethereum"
+        }
+    ],
+    "showSymbolLogo": true,
+    "colorTheme": "dark",
+    "isTransparent": false,
+    "displayMode": "adaptive",
+    "locale": "en"
+    }
+    </script>
+    </div>
+    """
+    components.html(Trading_view_ticker_tape, height=90)
     # Dictionary of famous Indian stock companies with their symbols
     stocks_with_symbols = {
         "Reliance Industries": "RELIANCE",
@@ -67,26 +105,25 @@ def fintech_app():
         stock_symbol = stocks_with_symbols[selected_stock]  # Retrieve the symbol for the selected stock
     else:
         stock_symbol = "NZDCAD" 
-    tradingview_widget_code = f"""
+    tradingview_chart_code = f"""
         <div class="tradingview-widget-container">
-            <div id="tradingview_{stock_symbol}"></div>
+            <div id="tradingview_chart_{stock_symbol}"></div>
             <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
             <script type="text/javascript">
             new TradingView.widget(
                 {{
-                    "container_id": "tradingview_{stock_symbol}",
+                    "container_id": "tradingview_chart_{stock_symbol}",
                     "symbol": "{stock_symbol}",
                     "interval": "D",
                     "width": "100%",
                     "height": "400",
-                    // Additional widget options
+                    // Additional chart widget options
                 }}
             );
             </script>
         </div>
         """
-    components.html(tradingview_widget_code, height=450)
-
+    components.html(tradingview_chart_code, height=450)
 
     col1, col2 = st.columns(2)
     # Stock information to prepend to Claude's prompt
